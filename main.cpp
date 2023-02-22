66c4fc2679b7e4aa913ddff9784452370a2fb92c
Fernando Lopez <flopez00032@gmail.com>
2023-02-22 10:56:56 -0600

    fixing comments ;

--- main.cpp
+++ main.cpp
@@ -1,13 +1,6 @@
 #include <iostream>
 using namespace std;
 
-// 
-
-// #include <SFML/Graphics.hpp>
-
-// #include <SFML/Graphics.hpp>
-
-
 int main(){
 
     // sf::RenderWindow windows(sf::VideoMode(800,600), "Snamke Game");
