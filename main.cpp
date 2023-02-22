6f55ce7b8025dbb1bf178e858502c92ca345ade6
Fernando Lopez <flopez00032@gmail.com>
2023-02-22 10:35:11 -0600

    how to include sfml and build with cmake

--- main.cpp
+++ main.cpp
@@ -1,16 +1,19 @@
-// #include <iostream>
-// using namespace std;
+#include <iostream>
+using namespace std;
 
 // 
 
-#include <SFML/Graphics.hpp>
+// #include <SFML/Graphics.hpp>
+
+// #include <SFML/Graphics.hpp>
+
 
 int main(){
 
-    sf::RenderWindow windows(sf::VideoMode(800,600), "Snamke Game");
+    // sf::RenderWindow windows(sf::VideoMode(800,600), "Snamke Game");
     
 
     // sanity check 
-    // cout << "hello world" << endl;
+    cout << "hello world again " << endl;
     return 0;
 }
