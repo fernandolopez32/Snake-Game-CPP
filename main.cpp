2fb037f0cb591ce1a733443c52331d0fecd4863d
Fernando Lopez <flopez00032@gmail.com>
2023-02-24 16:39:33 -0600

    displaying a shape on a window using SFML

--- main.cpp
+++ main.cpp
@@ -1,19 +1,4 @@
-// #include <iostream>
-// using namespace std;
-
-// int main(){
-
-//     // sf::RenderWindow windows(sf::VideoMode(800,600), "Snamke Game");
-    
-
-//     // sanity check 
-//     cout << "hello world again " << endl;
-//     return 0;
-// }
-
-// #include <SFML/Graphics.hpp>
-
-#include<SFML/Graphics.hpp>
+#include <SFML/Graphics.hpp>
 
 int main()
 {
@@ -37,3 +22,44 @@ int main()
 
     return 0;
 }
+
+
+// #include <iostream>
+// using namespace std;
+
+// int main(){
+
+//     // sf::RenderWindow windows(sf::VideoMode(800,600), "Snamke Game");
+    
+
+//     // sanity check 
+//     cout << "hello world again " << endl;
+//     return 0;
+// }
+
+// #include <SFML/Graphics.hpp>
+
+// #include<SFML/Graphics.hpp>
+
+// int main()
+// {
+//     sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+//     sf::CircleShape shape(100.f);
+//     shape.setFillColor(sf::Color::Green);
+
+//     while (window.isOpen())
+//     {
+//         sf::Event event;
+//         while (window.pollEvent(event))
+//         {
+//             if (event.type == sf::Event::Closed)
+//                 window.close();
+//         }
+
+//         window.clear();
+//         window.draw(shape);
+//         window.display();
+//     }
+
+//     return 0;
+// }
