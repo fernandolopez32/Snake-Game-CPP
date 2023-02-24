112560b77e3e35defbe0c6e067439c8ec6416130
Fernando Lopez <flopez00032@gmail.com>
2023-02-24 11:55:40 -0600

    configs

--- main.cpp
+++ main.cpp
@@ -1,12 +1,39 @@
-#include <iostream>
-using namespace std;
+// #include <iostream>
+// using namespace std;
 
-int main(){
+// int main(){
 
-    // sf::RenderWindow windows(sf::VideoMode(800,600), "Snamke Game");
+//     // sf::RenderWindow windows(sf::VideoMode(800,600), "Snamke Game");
     
 
-    // sanity check 
-    cout << "hello world again " << endl;
+//     // sanity check 
+//     cout << "hello world again " << endl;
+//     return 0;
+// }
+
+// #include <SFML/Graphics.hpp>
+
+#include<SFML/Graphics.hpp>
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
     return 0;
 }
